b927a1bd1592870e8256d988fa9b9ba2ed4aa11d
Brian Hsing <brian.hsing.tw@gmail.com>
2020-09-23 16:34:57 +0800

    加入專案檔案。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace web092316.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
